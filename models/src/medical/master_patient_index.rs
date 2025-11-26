63f0d185b584743ba6d24c8dd14db35ee32671cb
Dmitry Roitman <dmitryro@gmail.com>
2025-11-26 09:48:07 -0500

    Added MPI logic

--- models/src/medical/master_patient_index.rs
+++ models/src/medical/master_patient_index.rs
@@ -1,7 +1,7 @@
 // models/src/medical/master_patient_index.rs
 use chrono::{DateTime, Utc};
-// FIX: Changed `crate::models::{...}` to `crate::{...}`
 use crate::{Vertex, ToVertex, identifiers::Identifier};
+use crate::medical::Address;
 
 #[derive(Debug, Clone)]
 pub struct MasterPatientIndex {
@@ -11,7 +11,7 @@ pub struct MasterPatientIndex {
     pub last_name: Option<String>,
     pub date_of_birth: Option<DateTime<Utc>>,
     pub gender: Option<String>,
-    pub address: Option<String>,
+    pub address: Option<Address>,
     pub contact_number: Option<String>,
     pub email: Option<String>,
     pub social_security_number: Option<String>,
@@ -44,7 +44,7 @@ impl ToVertex for MasterPatientIndex {
             v.add_property("gender", val);
         }
         if let Some(ref val) = self.address {
-            v.add_property("address", val);
+            v.add_property("address", &serde_json::to_string(val).expect("Address serialize"));
         }
         if let Some(ref val) = self.contact_number {
             v.add_property("contact_number", val);
@@ -66,5 +66,73 @@ impl ToVertex for MasterPatientIndex {
         v.add_property("updated_at", &self.updated_at.to_rfc3339());
 
         v
+    }
+}
+
+impl MasterPatientIndex {
+    /// Build a struct from a Vertex (mirror of ClinicalNote pattern)
+    pub fn from_vertex(vertex: &Vertex) -> Option<Self> {
+        if vertex.label.as_ref() != "MasterPatientIndex" {
+            return None;
+        }
+        Some(MasterPatientIndex {
+            id: vertex.properties.get("id")?.as_str()?.parse().ok()?,
+            patient_id: vertex
+                .properties
+                .get("patient_id")
+                .and_then(|v| v.as_str()?.parse().ok()),
+            first_name: vertex
+                .properties
+                .get("first_name")
+                .and_then(|v| v.as_str().map(String::from)),
+            last_name: vertex
+                .properties
+                .get("last_name")
+                .and_then(|v| v.as_str().map(String::from)),
+            date_of_birth: vertex
+                .properties
+                .get("date_of_birth")
+                .and_then(|v| DateTime::parse_from_rfc3339(v.as_str()?).ok())
+                .map(|dt| dt.with_timezone(&Utc)),
+            gender: vertex
+                .properties
+                .get("gender")
+                .and_then(|v| v.as_str().map(String::from)),
+            // NEW: deserialise Address from JSON string
+            address: vertex.properties.get("address")
+                .and_then(|v| v.as_str())
+                .and_then(|s| serde_json::from_str::<Address>(s).ok()),
+            contact_number: vertex
+                .properties
+                .get("contact_number")
+                .and_then(|v| v.as_str().map(String::from)),
+            email: vertex
+                .properties
+                .get("email")
+                .and_then(|v| v.as_str().map(String::from)),
+            social_security_number: vertex
+                .properties
+                .get("ssn")
+                .and_then(|v| v.as_str().map(String::from)),
+            match_score: vertex
+                .properties
+                .get("match_score")
+                .and_then(|v| v.as_str()?.parse().ok()),
+            match_date: vertex
+                .properties
+                .get("match_date")
+                .and_then(|v| DateTime::parse_from_rfc3339(v.as_str()?).ok())
+                .map(|dt| dt.with_timezone(&Utc)),
+            created_at: DateTime::parse_from_rfc3339(
+                vertex.properties.get("created_at")?.as_str()?
+            )
+            .ok()?
+            .with_timezone(&Utc),
+            updated_at: DateTime::parse_from_rfc3339(
+                vertex.properties.get("updated_at")?.as_str()?
+            )
+            .ok()?
+            .with_timezone(&Utc),
+        })
     }
 }
