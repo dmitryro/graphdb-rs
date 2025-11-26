c3cf970f55e5b476aec21ae722442e855050faf5
Dmitry Roitman <dmitryro@gmail.com>
2025-11-26 10:42:52 -0500

    Basic logic for patient journey added.

--- medical_knowledge/src/patient_journey/mod.rs
+++ medical_knowledge/src/patient_journey/mod.rs
@@ -1,7 +1,11 @@
 // medical_knowledge/src/patient_journey/mod.rs
-
 pub mod patient_journey;
 
-pub use patient_journey::*;
-
-
+pub use patient_journey::{
+    PatientJourneyService,
+    PatientJourney,
+    ActivePathway,
+    JourneyDeviation,
+    JourneyMilestone,
+    PATIENT_JOURNEY_SERVICE,
+};
