1b4e22bc0153487d57a0bfc87585f5b0c2a156e8
Dmitry Roitman <dmitryro@gmail.com>
2025-11-27 17:01:22 -0500

    Added fhir service to medical knowledge crate.

---
+++ server/src/cli/handlers_connectors.rs
@@ -0,0 +1,1 @@
+// server/src/cli/handlers_connectors.rs
---
+++ server/src/cli/handlers_drug_interactions.rs
@@ -0,0 +1,7 @@
+// server/src/cli/handlers_drug_interactions.rs
+use  medical_knowledge::drug_interaction_knowledge::{
+    DrugInteractionKnowledgeService,
+    DrugInteractionAlert,
+    InteractionSeverity,
+    DRUG_INTERACTION_SERVICE,
+};
